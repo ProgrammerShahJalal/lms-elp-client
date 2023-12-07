a1e7480ebaaa46fd56a7b56e903dde9cfde8dc64
saharabanu <saharabanungn@gmail.com>
2023-12-07 15:47:26 +0600

    user profile edit page done

--- elp-client/src/app/(dashboard)/user/profile/edit/[id]/page.js
+++ elp-client/src/app/(dashboard)/user/profile/edit/[id]/page.js
@@ -1,14 +1,94 @@
-import DashNavbar from "@/components/dashboard/DashNavbar"
-import Link from "next/link"
+"use client";
 
+import { useForm } from "react-hook-form";
 
 const UserProfileEditPage = () => {
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => console.log(data);
   return (
-    <div>
-    <DashNavbar/>
-    <Link href='/user/profile'>go profile</Link>
-    </div>
-  )
-}
+    <>
+    <div className="bg-white rounded-lg py-6 border border-gray-200 px-10 ">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          {...register("name")}
+          type="text"
+          placeholder="আপনার নাম"
+          className="input input-bordered input-md w-full max-w-xs mr-5 outline-none mb-4"
+        />
+        <input
+          {...register("email")}
+          type="email"
+          placeholder="আপনার ইমেইল"
+          className="input input-bordered input-md w-full max-w-xs mb-4 mr-5"
+        />
+        <input
+          {...register("phoneNumber")}
+          type="number"
+          placeholder="আপনার মোবাইল নাম্বার"
+          className="input input-bordered input-md w-full max-w-xs mb-4 mr-5"
+        />
+        <input
+          {...register("profession")}
+          type="text"
+          placeholder="আপনার পেশা"
+          className="input input-bordered input-md w-full max-w-xs mb-4 mr-5"
+        />
+        <input
+          {...register("address")}
+          type="text"
+          placeholder="আপনার ঠিকানা"
+          className="input input-bordered input-md w-full max-w-xs mb-4 mr-5"
+        />
+        <input
+          {...register("bateofbirth")}
+          type="date"
+          placeholder="আপনার জন্ম তারিখ"
+          className="input input-bordered input-md w-full max-w-xs mb-4 mr-5"
+        />
 
-export default UserProfileEditPage
+        <select
+          className="select select-bordered w-full max-w-xs mb-4 mr-5"
+          {...register("gender")}
+          placeholder="gender"
+        >
+          <option disabled selected>
+            Male
+          </option>
+          <option>Male</option>
+          <option>Female</option>
+          <option>Others</option>
+        </select>
+
+        <select
+          className="select select-bordered w-full max-w-xs mb-4 mr-5"
+          {...register("gender")}
+        >
+          <option  selected>
+            Primary Job
+          </option>
+          <option >
+            Primary Job
+          </option>
+          <option>NTRCA Job</option>
+          <option>BCS Job</option>
+          <option>Others</option>
+        </select>
+        <textarea
+          {...register("desc")}
+          placeholder="নিজের সম্পর্কে লিখুন"
+          className="textarea textarea-bordered textarea-lg w-full max-w-xs mb-4"
+        ></textarea>
+        <br />
+
+        <input
+          type="submit"
+          value="সেভ করুন"
+          className="flex items-center gap-3 bg-sky-900 text-white transition-all  hover:bg-blue-900 cursor-pointer px-5 py-2 rounded"
+        />
+      </form>
+    </div>
+    </>
+  );
+};
+
+export default UserProfileEditPage;
--- elp-client/src/components/dashboard/userDashboard/UserWelcome.js
+++ elp-client/src/components/dashboard/userDashboard/UserWelcome.js
@@ -1,11 +1,42 @@
-import React from 'react'
+'use client'
+
+import { useEffect, useState } from "react";
 
 const UserWelcome = () => {
+  const [timeOfDay, setTimeOfDay] = useState('');
+
+  useEffect(() => {
+    const updateWelcomeMessage = () => {
+      const currentHour = new Date().getHours();
+
+      if (currentHour >= 6 && currentHour < 12) {
+        setTimeOfDay('শুভ সকাল');
+      } else if (currentHour >= 12 && currentHour < 17) {
+        setTimeOfDay('শুভ অপরাহ্ন');
+      } else if (currentHour >= 17 && currentHour < 20) {
+        setTimeOfDay('শুভ সন্ধ্যা');
+      } else {
+        setTimeOfDay('শুভ রাত');
+      }
+    };
+
+    updateWelcomeMessage(); 
+
+   
+    const intervalId = setInterval(updateWelcomeMessage, 60000);
+
+    return () => clearInterval(intervalId); 
+  }, []);
+
+
+
   return (
     <div className='bg-white rounded-lg py-8 w-100 border border-gray-200'>
      <div className='px-10'>
-        <h2 className='text-2xl font-bold pb-3 text-cyan-900'>শুভ সন্ধ্যা Jesmin</h2>
-        <h5 className='text-cyan-900 text-lg'>লাইভ ক্লাসের খুঁটিনাটি সব জেনে নাও তোমার লার্নিং প্লাটফর্ম ড্যাশবোর্ডে</h5>
+     <h2 className='text-2xl font-bold pb-3 text-cyan-900'>
+      {`${timeOfDay} Jesmin`}
+    </h2>
+        <h5 className='text-cyan-900 text-lg'>লাইভ ক্লাসের খুঁটিনাটি সব জেনে নাও তোমার <span className='text-cyan-900'>ইজি লার্নিং প্লাটফর্ম</span> ড্যাশবোর্ডে</h5>
      </div>
        
 
