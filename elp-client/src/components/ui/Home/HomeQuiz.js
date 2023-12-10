a4d222fdd9d2fdf60304247c72cbaa9f44c4379a
saharabanu <saharabanungn@gmail.com>
2023-12-10 17:32:08 +0600

    quiz done

--- elp-client/src/components/ui/Home/HomeQuiz.js
+++ elp-client/src/components/ui/Home/HomeQuiz.js
@@ -1,3 +1,4 @@
+import Link from "next/link"
 
 
 const HomeQuiz = () => {
@@ -7,7 +8,9 @@ const HomeQuiz = () => {
               <div className="space-y-4">
                 <h2 className="lg:text-3xl md:text-2xl font-semibold">বুঝতে পারছেন না কোথা থেকে শুরু করবেন?</h2>
                 <h5 className="lg:text-lg py-4 mb-3">চিন্তার কিছু নেই! আমাদের কুইজ সেগমেন্ট আপনাকে পৌছে দেবে সঠিক সিদ্ধান্তে।।</h5>
+                <Link href="/basicQuiz">
                 <button className="flex items-center gap-3 bg-yellowPrimary text-white ease-out duration-500 transition-all hover:bg-cyanPrimary cursor-pointer px-6 py-3 rounded "> কুইজ শুরু করুন</button>
+                </Link>
                
                 
               </div>
---
+++ elp-client/src/components/ui/Home/Quiz.js
@@ -0,0 +1,54 @@
+'use client'
+import { useState } from 'react';
+
+const Quiz = ({ quizData, onSubmit }) => {
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [userAnswers, setUserAnswers] = useState(Array(quizData.length).fill(null));
+
+  const handleOptionSelect = (selectedOption) => {
+    const updatedAnswers = [...userAnswers];
+    updatedAnswers[currentQuestion] = selectedOption;
+    setUserAnswers(updatedAnswers);
+  };
+
+  const handleNext = () => {
+    setCurrentQuestion((prevQuestion) => prevQuestion + 1);
+  };
+
+  const handlePrevious = () => {
+    setCurrentQuestion((prevQuestion) => prevQuestion - 1);
+  };
+
+  const isLastQuestion = currentQuestion === quizData.length - 1;
+
+  return (
+    <div className='space-y-7 text-center'>
+      <h2 className="text-xl font-bold pt-5">Question {currentQuestion + 1}</h2>
+      <p className=''>{quizData[currentQuestion].question}</p>
+      <div>
+        {quizData[currentQuestion].options.map((option, index) => (
+          <label key={index}>
+            <input
+              type="radio"
+              name={`question${currentQuestion}`}
+              value={option}
+              checked={userAnswers[currentQuestion] === option}
+              onChange={() => handleOptionSelect(option)}
+            />
+            {option}
+          </label>
+        ))}
+      </div>
+      <div>
+        {currentQuestion > 0 && <button className="bg-bluePrimary text-white py-2 px-4 transition-all duration-300 rounded hover:bg-cyanPrimary mr-5"  onClick={handlePrevious}>Previous</button>}
+        {isLastQuestion ? (
+          <button onClick={() => onSubmit(userAnswers)} className="bg-yellowPrimary text-white py-2 px-4 transition-all duration-300 rounded hover:bg-bluePrimary" >Submit</button>
+        ) : (
+          <button onClick={handleNext} className="bg-bluePrimary text-white py-2 px-4 transition-all duration-300 rounded hover:bg-cyanPrimary" >Next</button>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Quiz;
