aef5494d229b6e17ab0ff3129ea993a4af122704
saharabanu <saharabanungn@gmail.com>
2023-12-15 09:57:15 +0600

    about page done

--- elp-client/src/components/pages/about/AboutUs.js
+++ elp-client/src/components/pages/about/AboutUs.js
@@ -1,12 +1,16 @@
 import Commonbanner from "@/components/banners/Commonbanner";
 import Teachers from "./Teachers";
 import FirstPartAbout from "./FirstPartAbout";
+import OurMission from "./OurMission";
+import ChooseUs from "./ChooseUs";
 
 const AboutUs = () => {
   return (
     <div className=" ">
       <Commonbanner />
       <FirstPartAbout/>
+      <OurMission/>
+      <ChooseUs/>
       <Teachers/>
     </div>
   );
