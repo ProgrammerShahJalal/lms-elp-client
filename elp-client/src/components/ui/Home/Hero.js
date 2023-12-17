6d7550b11e7c4036ee43e29323df4c108c44528d
unknown <Monirul-Jim>
2023-12-17 13:01:30 +0600

    added course

--- elp-client/src/components/ui/Home/Hero.js
+++ elp-client/src/components/ui/Home/Hero.js
@@ -1,13 +1,15 @@
+'use client'
 import React from "react";
 import banner from "../../../assets/images/banner.png";
 import Image from "next/image";
+import Link from "next/link";
 
 const Hero = () => {
   return (
     <div>
       <div className="banner bg-cyanPrimary ">
         <div className="hero-content flex-col lg:flex-row-reverse ">
-          <Image src={banner} className="max-w-sm rounded-lg shadow-2xl" alt="hero img"/>
+          <Image src={banner} className="max-w-sm rounded-lg shadow-2xl" alt="hero img" />
           <div className="">
             <h1 className="text-5xl font-bold leading-relaxed text-cyanPrimary">
               নিজের মত শিখুন, <br />
@@ -18,9 +20,9 @@ const Hero = () => {
               <span className="opacity-0">ক্যারিয়ার ট্র্যাক কোর্সগুলোই যথেষ্ট।</span>
             </p>
             <p className="pb-6 font-medium text-lg"> ক্যারিয়ার ট্র্যাক কোর্সগুলোই যথেষ্ট।</p>
-            <button className="bg-bluePrimary text-white py-3 px-4 transition-all duration-300 rounded hover:bg-cyanPrimary">
+            <Link href={'/courses'} className="bg-bluePrimary text-white py-3 px-4 transition-all duration-300 rounded hover:bg-cyanPrimary">
               কোর্সগুলো দেখুন
-            </button>
+            </Link >
           </div>
         </div>
       </div>
