2898ed7ecde5af7d417b057769741b64d01c218f
saharabanu <saharabanungn@gmail.com>
2023-12-23 22:37:19 +0600

    navbar doepdown done

--- elp-client/src/components/shared/Navbar.js
+++ elp-client/src/components/shared/Navbar.js
@@ -7,33 +7,36 @@ import Link from "next/link";
 import Image from "next/image";
 import { IoIosArrowDown } from "react-icons/io";
 import ToggleTheme from "./ToggleTheme";
-import { getUserInfo, isLoggedIn, removeUserInfo } from "@/services/auth.service";
+import {
+  getUserInfo,
+  isLoggedIn,
+  removeUserInfo,
+} from "@/services/auth.service";
 import { authKey } from "@/constants/storage";
 import { useRouter } from "next/navigation";
 import { useGetSingleUserQuery } from "@/redux/api/authApi";
 import { useGetAllCategoriesQuery } from "@/redux/api/categoryApi";
 import { IoCartOutline } from "react-icons/io5";
 import { useSelector } from "react-redux";
+import avatar from "../../assets/images/avatar.png";
 
 const Navbar = () => {
-
   // const ClickableDropdown = () => {
   //   const [isOpen, setIsOpen] = useState(false);
-  
+
   //   const toggleDropdown = () => {
   //     setIsOpen(!isOpen);
   //   };
 
-  const { data:courseCategoryData } = useGetAllCategoriesQuery();
+  const { data: courseCategoryData } = useGetAllCategoriesQuery();
   const categoriesData = courseCategoryData?.categories;
   const { books } = useSelector((state) => state.cart);
-  
+
   const userLoggedIn = isLoggedIn();
   const router = useRouter();
-  const { userId} = getUserInfo();
-  
+  const { userId } = getUserInfo();
+
   const { data } = useGetSingleUserQuery(userId);
-  
 
   // logout
 
@@ -63,8 +66,11 @@ const Navbar = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
+  const [isOpen, setIsOpen] = useState(false);
 
-  
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
 
   useEffect(() => {
     const handleScroll = () => {
@@ -81,11 +87,12 @@ const Navbar = () => {
     };
   }, []);
 
-  const coursesLink =  categoriesData?.map((item)=>{  
+  const coursesLink = categoriesData?.map((item) => {
     return {
-      sublink: `${item?.name}`, subpath: `/courses/category/${item?._id}`
-    }
-  })
+      sublink: `${item?.title}`,
+      subpath: `/courses/category/${item?._id}`,
+    };
+  });
 
   const commonRoutes = [
     { link: "হোম", path: "/" },
@@ -96,30 +103,38 @@ const Navbar = () => {
     },
     { link: "আমাদের সম্পর্কে", path: "about" },
     { link: "যোগাযোগ", path: "contact" },
-    
-    
   ];
-  const navItems = userLoggedIn
-  ? [...commonRoutes, { link: "প্রোফাইল", path: "profile" }]
-  : commonRoutes;
+  // const navItems = userLoggedIn
+  // ? [...commonRoutes, { link: "প্রোফাইল", path: "profile" }]
+  // : commonRoutes;
   return (
     <header className="w-full bg-white md:bg-transparent sticky top-0 left-0 right-0 z-10 border-b border-b-gray-200 shadow-lg">
       <nav
         className={`py-4  px-4 ${
-          isSticky ? " bg-white sticky top-0 left-0 right-0 border-b duration-300" : ""
+          isSticky
+            ? " bg-white sticky top-0 left-0 right-0 border-b duration-300"
+            : ""
         }`}
       >
         <div className="flex justify-between items-center text-base gap-8">
-          <Link href="/" className="text-lg font-semibold flex items-center space-x-2">
-            <Image src={logo} alt="logo" className="w-14 inline-block items-center" />
-            <span className="text-cyanPrimary hover:text-bluePrimary">ইজি লার্নিং প্লাটফর্ম</span>
+          <Link
+            href="/"
+            className="text-lg font-semibold flex items-center space-x-2"
+          >
+            <Image
+              src={logo}
+              alt="logo"
+              className="w-14 inline-block items-center"
+            />
+            <span className="text-cyanPrimary hover:text-bluePrimary">
+              ইজি লার্নিং প্লাটফর্ম
+            </span>
           </Link>
 
           {/* nav for large device*/}
-          
 
           <ul className="md:flex space-x-5 hidden">
-            {navItems.map(({ link, path, dropdown }) => (
+            {commonRoutes.map(({ link, path, dropdown }) => (
               <div key={path}>
                 {dropdown ? (
                   <div
@@ -162,55 +177,77 @@ const Navbar = () => {
           </ul>
 
           {/* btn for large device */}
-          <div className="space-x-2 hidden lg:flex items-center">
+          <div className="space-x-4 hidden lg:flex items-center">
             <ToggleTheme />
 
-               
-<button id="dropdownHoverButton" data-dropdown-toggle="dropdownHover" data-dropdown-trigger="hover" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">Dropdown hover <svg className="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
-</svg>
-</button>
+            <Link href="/cart" className="flex items-center">
+              <IoCartOutline className="text-2xl font-bold" />{" "}
+              <sup className="text-md font-bold">{books?.length}</sup>
+            </Link>
 
-{/* <!-- Dropdown menu --> */}
-<div id="dropdownHover" className="z-10 hidden bg-red-900 divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
-    <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownHoverButton">
-      <li>
-        <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>
-      </li>
-      <li>
-        <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Settings</a>
-      </li>
-      <li>
-        <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Earnings</a>
-      </li>
-      <li>
-        <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Sign out</a>
-      </li>
-    </ul>
-</div>
-
-
-
-
-            {userLoggedIn ? (
-              <>
-              <p>{data?.name}</p>
-              <Link href="/cart" className="flex items-center"><IoCartOutline className="text-2xl font-bold" /> <sup className="text-md font-bold">{books?.length}</sup></Link>
+            {/* <!-- Dropdown menu --> */}
+            <div className="relative inline-block text-left">
               <button
-                onClick={logout}
-                className="bg-bluePrimary text-white py-2 px-4 transition-all duration-300 rounded hover:bg-cyanPrimary"
+                type="button"
+                onClick={toggleDropdown}
+                id="dropdownHoverButton"
+                className=" bg-gray-200 rounded-full p-2"
+                aria-haspopup="true"
+                aria-expanded="true"
               >
-                লগআউট
+                <Image
+                  src={avatar}
+                  width={20}
+                  height={50}
+                  className="rounded "
+                />
               </button>
-              </>
-            ) : (
-              <Link
-                href="/login"
-                className="hidden lg:flex items-center text-cyanPrimary hover:text-bluePrimary font-bold"
+
+              <div
+                className={`${
+                  isOpen ? "block" : "hidden"
+                } absolute z-10 mt-4 w-44 left-[-130px] bg-gray-200 divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700`}
               >
-                লগইন 
-              </Link>
-            )}
+                <ul className="py-2 text-sm text-gray-700 dark:text-gray-200">
+                  {userLoggedIn ? (
+                    <>
+                      <li>
+                        <Link
+                          href="/"
+                          className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                        >
+                          <p className="text-lg font-bold">{data?.name}</p>
+                        </Link>
+                      </li>
+                      <li>
+                        <Link
+                          href="/profile"
+                          className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                        >
+                          Profile
+                        </Link>
+                      </li>
+
+                      <li>
+                        <button
+                          onClick={logout}
+                          className="bg-bluePrimary text-white py-2 px-4 transition-all duration-300 rounded hover:bg-cyanPrimary"
+                        >
+                          লগআউট
+                        </button>
+                      </li>
+                    </>
+                  ) : (
+                    <Link
+                      href="/login"
+                      className="hidden lg:flex items-center text-cyanPrimary hover:text-bluePrimary font-bold"
+                    >
+                      লগইন
+                    </Link>
+                  )}
+                </ul>
+              </div>
+            </div>
           </div>
 
           {/* menu btn for only mobile devices */}
@@ -227,14 +264,17 @@ const Navbar = () => {
         {/* nav items for mobile devices */}
         <div
           className={`space-y-4  mt-16 py-7 bg-bluePrimary ${
-            isMenuOpen ? "block fixed top-0 right-0 left-0 text-center" : "hidden"
+            isMenuOpen
+              ? "block fixed top-0 right-0 left-0 text-center"
+              : "hidden"
           }`}
         >
-          {navItems.map(({ link, path, dropdown }) => (
+          <ToggleTheme />
+          {commonRoutes.map(({ link, path, dropdown }) => (
             <div key={path}>
               {dropdown ? (
                 <div
-                  className="relative inline-block  hover:text-bluePrimary font-bold cursor-pointer text-white"
+                  className="relative inline-block  hover:text-yellowPrimary font-bold cursor-pointer text-white"
                   onClick={toggleCoursesDropdown}
                   //   onMouseEnter={openCoursesDropdown}
                   //   onMouseLeave={closeCoursesDropdown}
@@ -270,17 +310,31 @@ const Navbar = () => {
               )}
             </div>
           ))}
+          <div className="flex justify-center">
+          <Link href="/cart" className="flex items-center"><IoCartOutline className="text-2xl font-bold text-white" /> <sup className="text-md font-bold text-white">{books?.length}</sup></Link>
+          </div>
           <div className=" ">
-            <a href="/login" className=" text-white hover:text-bluePrimary font-bold">
-              লগইন করুন
-            </a>
-            <br />
-            <Link
-              href="/register"
-              className="bg-bluePrimary text-white py-2 px-4 transition-all duration-300 rounded ml-[-20px] hover:bg-cyanPrimary font-bold"
-            >
-              রেজিস্টার
-            </Link>
+          {userLoggedIn ? (
+              <>
+              <p className="font-bold text-lg text-white">{data?.name}</p>
+              
+              <button
+                onClick={logout}
+                className="bg-bluePrimary text-white py-2 px-4 transition-all duration-300 rounded hover:bg-cyanPrimary"
+              >
+                লগআউট
+              </button>
+              </>
+            ) : (
+              <Link
+                href="/login"
+                className="hidden lg:flex items-center text-white hover:text-white font-bold"
+              >
+                লগইন 
+              </Link>
+            )}
+           
+            
           </div>
         </div>
       </nav>
