8af0cbfa27834f4dfbd78a064724b40921d280e6
saharabanu <saharabanungn@gmail.com>
2023-12-31 17:25:20 +0600

    done

--- elp-client/src/components/ui/Home/Quiz.js
+++ elp-client/src/components/ui/Home/Quiz.js
@@ -4,6 +4,11 @@ import { useState } from 'react';
 const Quiz = ({ quizData, onSubmit }) => {
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [userAnswers, setUserAnswers] = useState(Array(quizData.length).fill(null));
+  const [clickedOption, setClickedOption] = useState(null);
+  const handleOptionClick = (option) => {
+    setClickedOption(option);
+    handleOptionSelect(option);
+  };
 
   const handleOptionSelect = (selectedOption) => {
     const updatedAnswers = [...userAnswers];
@@ -26,26 +31,33 @@ const Quiz = ({ quizData, onSubmit }) => {
       <h2 className="text-xl font-bold pt-5">Question {currentQuestion + 1}</h2>
       <p className='text-lg'>{quizData[currentQuestion].question}</p>
       <div className='grid lg:grid-cols-4 gap-4 px-4 '>
-        {quizData[currentQuestion].options.map((option, index) => (
-          <label key={index} className='border py-4 rounded hover:shadow-lg cursor-pointer hover:text-white hover:bg-bluePrimary'>
-            <input 
-              type="radio" required
-              name={`question${currentQuestion}`}
-              value={option}
-              checked={userAnswers[currentQuestion] === option}
-              onChange={() => handleOptionSelect(option)}
-              className='mr-2'
-            />
-            {option}
-          </label>
-        ))}
+      {quizData[currentQuestion].options.map((option, index) => (
+        <label
+          key={index}
+          className={`border py-4 rounded cursor-pointer ${
+            clickedOption === option ? 'shadow-lg text-white bg-bluePrimary' : 'hover:shadow-lg hover:text-white hover:bg-bluePrimary'
+          }`}
+          onClick={() => handleOptionClick(option)}
+        >
+          <input
+            type="radio"
+            required
+            name={`question${currentQuestion}`}
+            value={option}
+            checked={userAnswers[currentQuestion] === option}
+            onChange={() => handleOptionSelect(option)}
+            className='mr-2'
+          />
+          {option}
+        </label>
+      ))}
       </div>
       <div>
         {currentQuestion > 0 && <button className="bg-bluePrimary text-white py-2 px-4 transition-all duration-300 rounded hover:bg-cyanPrimary mr-5"  onClick={handlePrevious}>Previous</button>}
         {isLastQuestion ? (
           <button onClick={() => onSubmit(userAnswers)} className="bg-yellowPrimary text-white py-2 px-4 transition-all duration-300 rounded hover:bg-bluePrimary" >Submit</button>
         ) : (
-          <button onClick={handleNext} className="bg-bluePrimary text-white py-2 px-4 transition-all duration-300 rounded hover:bg-cyanPrimary" >Next</button>
+          <button onClick={handleNext}  className="bg-bluePrimary text-white py-2 px-4 transition-all duration-300 rounded hover:bg-cyanPrimary" >Next</button>
         )}
       </div>
     </div>
