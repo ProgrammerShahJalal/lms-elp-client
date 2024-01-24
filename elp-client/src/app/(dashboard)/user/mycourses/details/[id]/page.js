456ff494347d21ff68f78362c00d78aa1d7f1d49
ProgrammerShahJalal <you@example.com>
2024-01-24 19:36:32 +0600

    play pause fix

--- elp-client/src/app/(dashboard)/user/mycourses/details/[id]/page.js
+++ elp-client/src/app/(dashboard)/user/mycourses/details/[id]/page.js
@@ -18,6 +18,7 @@ const Play = () => {
   const id = params?.id;
   const { data: course } = useGetMyCourseVedioPlaylistQuery(id);
   const [videos, setVideos] = useState([]);
+  const [isPlaying, setPlaying] = useState(false);
   const apiKey = "AIzaSyBZ5EMCEWwIV7h7UVVbinObGvG3cFdLj58";
   const maxVideosToShow = 50;
   const extractPlaylistIdFromUrl = (url) => {
@@ -48,9 +49,11 @@ const Play = () => {
           setVideos(allVideos);
           setSelectedVideo(allVideos[0]?.videoId);
           setCurrentPlaylistIndex(0);
+          setLoading(false);
         }
       } catch (error) {
         console.error("Error fetching playlists:", error);
+        setLoading(false);
       }
     };
 
@@ -60,6 +63,7 @@ const Play = () => {
   const [currentPlaylistIndex, setCurrentPlaylistIndex] = useState(0);
 
   const showPreviousPlaylist = () => {
+    setPlaying(false);
     setCurrentPlaylistIndex((prevIndex) =>
       prevIndex === 0 ? videos.length - 1 : prevIndex - 1
     );
@@ -72,6 +76,7 @@ const Play = () => {
     );
   };
   const showNextPlaylist = () => {
+    setPlaying(false);
     setCurrentPlaylistIndex((prevIndex) =>
       prevIndex === videos.length - 1 ? 0 : prevIndex + 1
     );
@@ -145,7 +150,6 @@ const Play = () => {
     }
   };
 
-  const [isPlaying, setPlaying] = useState(false);
 
   const handleClick = () => {
     setPlaying((prevPlaying) => !prevPlaying);
@@ -161,20 +165,30 @@ const Play = () => {
     }
   };
 
+/* full content redering and then display the page */
+const [isLoading, setLoading] = useState(true);
 
 
   return (
-    <div className="grid grid-cols-1 gap-4 ">
-      <div className="aspect-w-16 aspect-h-9 md:aspect-w-4 md:aspect-h-3 relative">
+    <div className="grid grid-cols-1 gap-1 select-none">
+
+{isLoading ? (
+        <div className="text-center">
+          <p>Loading...</p>
+        </div>
+      ) : (
+        <div className="aspect-w-16 aspect-h-9 md:aspect-w-4 md:aspect-h-3 relative">
        
         <div className="static">
-        <div className="bg-white py-7 z-20 absolute top-0 text-green-500">
-          --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+        <div className="bg-white  py-3 px-36 md:px-96 z-20 absolute top-0 text-green-500 font-bold flex items-center">
+          <h2>TOP</h2>
+          <img className="w-12" src="https://i.ibb.co/qNjGQnB/2592258.png" alt="secure"/>
+          <h2>SECURE</h2>
         </div>
         </div>
 
         <div className=" relative">
-          <div className='bg-transparent top-0 bottom-1/2 py-80 absolute text-transparent'>
+          <div className='bg-transparent top-0 bottom-0 md:bottom-1/2 md:py-80 absolute text-transparent'>
           --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
         </div>
  
@@ -188,7 +202,7 @@ const Play = () => {
 
         <div className="grid grid-cols-2 mt-5 pb-3">
           {/* Custom play button */}
-            <div>
+            <div className="text-center">
             <img
             className="custom-play-button play-icon cursor-pointer w-10"
               src={
@@ -202,11 +216,14 @@ const Play = () => {
                 handleClick();
               }}
             />
+            <div className="text-start">
+            {isPlaying? <small>Pause</small> : <small>Play</small> }
+            </div>
             </div>
   
 
        {/* Custom volume controls with progress bar */}
-        <div className="custom-volume-controls">
+        <div className="custom-volume-controls text-center">
           <input
             type="range"
             min="0"
@@ -215,19 +232,24 @@ const Play = () => {
             onChange={(e) => handleVolumeChange(parseInt(e.target.value))}
             className="volume-slider"
           />
+          <p>Volume</p>
         </div>
         </div>
 
 
 
 
-        <div className="bg-white px-2 absolute bottom-36 text-green-600">
-        --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+        
+        <div className="bg-white  py-1 px-36 md:px-96 z-20 absolute bottom-44 text-green-500 font-bold flex items-center">
+          <h2>TOP</h2>
+          <img className="w-12" src="https://i.ibb.co/qNjGQnB/2592258.png" alt="secure"/>
+          <h2>SECURE</h2>
         </div>
+      
 
         <h2 className="text-base"> {videos && videos.length > 0 && videos[currentPlaylistIndex]?.title}</h2>
 
-        <small className="z-50 absolute font-bold top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-red-600 shadow-sm">
+        <small className="z-50 absolute font-bold top-1/2 left-1/2 bottom-4 transform -translate-x-1/2 -translate-y-1/2 text-red-600 shadow-sm">
           {" "}
           {data?.contact_no}
         </small>
@@ -248,9 +270,11 @@ const Play = () => {
             Next
           </button>
         </div>
-      </div>
 
-      <div className="overflow-y-auto max-h-96">
+      </div>
+      )}
+
+<div className="overflow-y-auto max-h-96">
         <YoutubePlaylist
           videos={videos}
           course={course}
@@ -258,6 +282,7 @@ const Play = () => {
           onVideoClick={onVideoClick}
         />
       </div>
+      
     </div>
   );
 };
