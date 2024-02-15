c90d80174a6e376e07d587116d804903bcc99929
unknown <Monirul-Jim>
2024-02-15 13:32:45 +0600

    admin add question by number

--- elp-client/src/components/dashboard/admin/AdminChangeWrittenStatus.js
+++ elp-client/src/components/dashboard/admin/AdminChangeWrittenStatus.js
@@ -29,7 +29,16 @@ const AdminChangeWrittenStatus = ({ refetch, quiz, i, handleDelete }) => {
             <td className="py-2 px-4 border-b text-center">{quiz?.mark}</td>
             <td className="py-2 px-4 border-b text-center">{quiz?.exam_id?.title}</td>
             <td className="py-2 px-4 border-b text-center">{quiz?.exam_id?.is_active.toString()}</td>
-            <td className="py-2 px-4 border-b"><button onClick={handleStatusChange} className="bg-green-500 text-white py-1 px-2 rounded-md">Change Status</button></td>
+            {/* <td className="py-2 px-4 border-b"><button onClick={handleStatusChange} className="bg-green-500 text-white py-1 px-2 rounded-md">Approved Exam</button></td> */}
+            <td className="py-2 px-4 border-b text-center md:table-cell">
+                <button
+                    onClick={handleStatusChange}
+                    className={`bg-${activeStatus ? 'green' : 'red'}-500 text-white py-1 px-2 rounded-md`}
+
+                >
+                    {activeStatus ? 'Approved Exam' : 'Pending Approval'}
+                </button>
+            </td>
             <td className="py-2 px-4 border-b text-center md:table-cell">
                 <Link href={`/admin/addquestions/edit/${quiz?.id}`}
                     className="bg-lime-600 text-white py-2 px-2 rounded-md"
--- elp-client/src/utils/jwt.js
+++ elp-client/src/utils/jwt.js
@@ -1,6 +1,6 @@
 import jwt_decode from "jwt-decode";
 // import { jwtDecode } from "jwt-decode";
 
-export const  decodedToken = (token) => {
+export const decodedToken = (token) => {
     return jwt_decode(token)
 };
