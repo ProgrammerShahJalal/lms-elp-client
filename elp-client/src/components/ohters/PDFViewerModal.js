433593c3fe39062e67d3a47f19c7c02e729b94b1
ProgrammerShahJalal <you@example.com>
2024-05-16 22:08:29 +0600

    disable shortcuts

--- elp-client/src/components/ohters/PDFViewerModal.js
+++ elp-client/src/components/ohters/PDFViewerModal.js
@@ -4,20 +4,23 @@ import { useEffect } from "react";
 
 const PDFViewerModal = ({ isOpen, onClose, pdfSrc }) => {
   useEffect(() => {
+    const disableShortcuts = (e) => {
+      if (
+        (e.ctrlKey && e.key === 's') || // Disable Save (Ctrl+S)
+        (e.ctrlKey && e.code === 'KeyS') || // Additional check for Save
+        (e.ctrlKey && e.key === 'p') || // Disable Print (Ctrl+P)
+        (e.ctrlKey && e.code === 'KeyP') || // Additional check for Print
+        (e.ctrlKey && e.key === 'c') || // Disable Copy (Ctrl+C)
+        (e.ctrlKey && e.code === 'KeyC') || // Additional check for Copy
+        (e.ctrlKey && e.shiftKey && e.key === 'i') || // Disable Developer Tools (Ctrl+Shift+I)
+        (e.key === 'F12') // Disable Developer Tools (F12)
+      ) {
+        e.preventDefault();
+      }
+    };
+
     if (isOpen) {
       const disableContextMenu = (e) => e.preventDefault();
-      const disableShortcuts = (e) => {
-        if (
-          (e.ctrlKey && e.key === 'u') || // Disable View Source (Ctrl+U)
-          (e.ctrlKey && e.key === 's') || // Disable Save (Ctrl+S)
-          (e.ctrlKey && e.key === 'p') || // Disable Print (Ctrl+P)
-          (e.ctrlKey && e.key === 'c') || // Disable Copy (Ctrl+C)
-          (e.ctrlKey && e.key === 'i') || // Disable Developer Tools (Ctrl+Shift+I)
-          (e.key === 'F12') // Disable Developer Tools (F12)
-        ) {
-          e.preventDefault();
-        }
-      };
 
       document.addEventListener('contextmenu', disableContextMenu);
       document.addEventListener('keydown', disableShortcuts);
@@ -35,7 +38,7 @@ const PDFViewerModal = ({ isOpen, onClose, pdfSrc }) => {
         <div className="fixed inset-0 flex items-center justify-center scroll-smooth select-none z-50">
           <div className="bg-white p-4 rounded-md h-full w-full relative">
             
-            {/* <div className="bg-white py-1 px-36 md:px-96 absolute top-0 right-0 text-green-500 font-bold flex items-center z-10">
+            <div className="bg-white py-1 px-36 md:px-96 absolute top-0 right-0 text-green-500 font-bold flex items-center z-10">
               <h2 className="text-center px-12 md:px-32">TOP</h2>
               <h2 className="text-center px-12 md:px-32">SECURE</h2>
               <img
@@ -43,7 +46,7 @@ const PDFViewerModal = ({ isOpen, onClose, pdfSrc }) => {
                 src="https://i.ibb.co/qNjGQnB/2592258.png"
                 alt="secure"
               />
-            </div> */}
+            </div>
 
 
             <button
